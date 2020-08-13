48203ad9531cfd8342495ff0a88677c941d4e471
Rija Raju <rajur@uwindsor.ca>
2020-08-13 19:42:52 -0400

    Environment updated

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
 app.config["PROPAGATE_EXCEPTION"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATION"]=False
 
-app.secret_key = "raju"
+app.secret_key = "****"
 api = Api(app)
 
 @app.before_first_request
